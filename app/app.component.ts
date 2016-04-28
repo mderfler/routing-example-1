837196607d1f71a602a2417b0fdb1844cb7486a6
unknown <mderfler@LT10372.catalystsolves.com>
2016-04-28 09:32:55 -0700

    commit

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core';
+import {RouteConfig,RouterOutlet,RouterLink} from 'angular2/router';
+
+import {AlbumComponent} from './album.component';
+import {AlbumsComponent} from './albums.component';
+import {ContactComponent} from './contact.component';
+
+@RouteConfig([
+	{path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true},
+	{path: '/albums/:id', name: 'Album', component: AlbumComponent}
+	{path: '/contact', name: 'Contact', component: ContactComponent},
+	{path: '/*other', name: 'Other', redirectTo: ['Albums']}
+])
+@Component({
+    selector: 'my-app',
+    templateUrl: '/app/app.component.html',
+    directives: [RouterOutlet,RouterLink]
+})
+export class AppComponent {
+}
